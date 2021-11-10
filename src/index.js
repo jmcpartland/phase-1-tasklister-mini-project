470e419e8b7e6a26971cecb63fc1da31e6a4915e
Joe McPartland <joemcpartland@gmail.com>
2021-11-10 07:59:23 -0700

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener('submit', newTask);
+
 });
+
+function newTask(event) {
+  event.preventDefault()
+  const taskDescription = document.getElementById("new-task-description");
+  const li = document.createElement('li')
+  li.innerText = taskDescription.value;
+  
+  addNewTask(li)
+  event.target.reset();
+};
+
+function addNewTask(task) {
+  document.getElementById("tasks").appendChild(task);
+};
